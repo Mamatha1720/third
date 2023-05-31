4d3ec8fcd44ad3d6bc633e180120db456b8705ab
Mamatha1720 <mamathat1621@gmail.com>
2023-05-31 23:58:04 +0530

    recent chnages 1

--- jav.js
+++ jav.js
@@ -20,114 +20,13 @@
 // //SELECTING METHODS
 
 // //GET ELEMENT BY ID
-// console.log(document.getElementById('header-title'));
-// var headerTitle = document.getElementById('header-title');
-// var header = document.getElementById('main-header');
-// //console.log(headerTitle);
-// // headerTitle.textContent='Hello';
-// // headerTitle.innerText='goodbye';
-// //console.log(headerTitle.textContent);
 
-// //headerTitle.innerHTML='<h3>hello</h3>';
-
-// //header.style.border='solid 4px black';
-
-
-// // //GETELEMENTSBYVCLASSNAME
-// // var items=document.getElementsByClassName('list-group-item');
-// // console.log(items);
-// // console.log(items[1]);
-
-// // items[1].textContent='ITEM 2';
-// // items[1].style.backgroundColor='red';
-
-// //items.stle.backgroundColor='yellow';
-
-
-// // for(var i=0;i<items.length;i++)
-// // {
-// //     items[i].style.backgroundColor='teal';
-// // }
-
-
-// // //GETELEMENT BY TAG NAME
-// // var li=document.getElementsByTagName('li');
-// // console.log(li);
-// // console.log(li[1]);
-// // li[1].style.fontweight='bold';
-// // li[1].textContent='Hello hi';
-// // li[1].style.backgroundColor='red';
-
-// // for(var i=0;i<li.length;i++)
-// // {
-// //     li[i].style.backgroundColor='grey';
-// // }
-
-// //QUERY SELECTOR//
-
-// // var header=document.querySelector('#main-header');
-// // header.style.borderbottom='solid 4px #ccc';
-
-// var input= document.querySelector('input');
-// input.value='hello world'
-
-// var submit= document.querySelector('input[type="submit"]');
-// submit.value="send"
-// //  var item=document.querySelector('.list-group-item');
-// //  item.style.color='red';
-
-//  var lastItem=document.querySelector('.list-group-item:last-child');
-//  lastItem.style.color='transparent';
-
-//  var secondItem=document.querySelector('.list-group-item:nth-child(2)');
-//  secondItem.style. color='green';
- 
-// //QUERYSELECTORSALL
-
-// // var titles=document.querySelectorAll('.title');
-// // console.log('titles');
-
-// // var odd=document.querySelectorAll('li:nth-child(odd)');
-// // var even=document.querySelectorAll('li:nth-child(even)');
-// // for(var i=0; i<odd.length;i++)
-// // {
-// //     odd[i].style.backgroundColor='green';
-// //     even[i].style.backgroundColor='aqua';
-// // }
-
-
-// // TRAVERSING THE DOM //
-
-// var itemList = document.querySelector('#items');
-
-// // parentNode
-
-//  console.log(itemList.parentNode);
-//  itemList.parentNode.style.backgroundColor = '#f4f4f4';
-//  console.log(itemList.parentNode.parentNode.parentNode);
-
-// // parentElement
-
-// console.log(itemList.parentElement);
-// itemlist.parentElement.style.backgroundColor = '#f4f4f4';
-// console.log(itemList.parentElement.parentElement.parentElement);
-
-//Filter Items
-
-function filterItems (e){
-// convert text to lowercase
-var text = e.target.value.toLowerCase();
-// Get lis
- var items = itemList.getElementsByTagName('li') ;
- // Convert to an array
-Array.from(items).forEach(function(item)
-{ 
-var itemName = item.firstChild.textContent;
-if(itemName.toLowerCase().indexOf(text) != -1)
-{
-item.style.display = 'block';
-}
-else{
-    item.style.display = 'none';
-});
-}
+console.log(document.getElementById('header-title'));
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+console.log(headerTitle);
+//headerTitle.textContent='Hello';
+//headerTitle.innerText='goodbye';
+//headerTitle.innerHTML='<h3>hello</h3>';
+console.log(headerTitle.textContent);
+header.style.border='solid 3px black';
