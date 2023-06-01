d0d5b5d67a89342ac8869e7ecb643c5bd54d3a26
Mamatha1720 <mamathat1621@gmail.com>
2023-06-01 14:33:00 +0530

    grey color

--- jav.js
+++ jav.js
@@ -1,25 +1,25 @@
 //EXAMINE THE DOCUMENT OBJECT//
 
-// //console.dir(document);
-// // console.log(document.domain);
-// // console.log(document.URL);
-// // console.log(document.title);
-// // //document.title=123;
+//console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// //document.title=123;
 
-// // console.log(document.doctype);
-// // console.log(document.head);
-// // console.log(document.body);
-// // console.log(document.all);
-// // console.log(document.all[10]);
-// // //document.all[10].textContent='hello';
-// // console.log(document.forms);
-// // console.log(document.links);
-// //console.log(document.images);
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// //document.all[10].textContent='hello';
+// console.log(document.forms);
+// console.log(document.links);
+//console.log(document.images);
 
 
-// //SELECTING METHODS
+//SELECTING METHODS
 
-// //GET ELEMENT BY ID
+//GET ELEMENT BY ID
 
 // console.log(document.getElementById('header-title'));
 // var headerTitle = document.getElementById('header-title');
@@ -33,13 +33,28 @@
 
 
 //GETELEMENTSBYVCLASSNAME
-var items=document.getElementsByClassName('list-group-item');
-console.log(items);
+//var items=document.getElementsByClassName('list-group-item');
+//console.log(items);
 //console.log(items[1]);
 //items[1].textContent='ITEM 2';
- items[2].style.backgroundColor='green';
+ //items[2].style.backgroundColor='green';
 // items.stle.backgroundColor='yellow';
- for(var i=0;i<items.length;i++)
- {
-   //items[i].style.backgroundColor='teal';
- }
+ //for(var i=0;i<items.length;i++)
+//  {
+//    //items[i].style.backgroundColor='teal';
+//  }
+
+
+//GETELEMENT BY TAG NAME
+var li=document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].style.fontweight='bold';
+//li[1].textContent='Hello';
+li[1].style.backgroundColor='red';
+
+for(var i=0;i<li.length;i++)
+{
+    li[i].style.backgroundColor='grey';
+}
+
