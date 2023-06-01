be219de19fad471a8a87f623604f1e926b5381da
Mamatha1720 <mamathat1621@gmail.com>
2023-06-01 14:50:55 +0530

    changed

--- jav.js
+++ jav.js
@@ -46,15 +46,49 @@
 
 
 //GETELEMENT BY TAG NAME
-var li=document.getElementsByTagName('li');
-console.log(li);
-console.log(li[1]);
-li[1].style.fontweight='bold';
-//li[1].textContent='Hello';
-li[1].style.backgroundColor='red';
+// var li=document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+// li[1].style.fontweight='bold';
+// //li[1].textContent='Hello';
+// li[1].style.backgroundColor='red';
 
-for(var i=0;i<li.length;i++)
+// for(var i=0;i<li.length;i++)
+// {
+//     li[i].style.backgroundColor='grey';
+// }
+
+
+//QUERYSELECTOR//
+
+// var header=document.querySelector('#main-header');
+// header.style.borderbottom='solid 4px #ccc';
+
+// var input= document.querySelector('input');
+// input.value='hello world'
+
+var submit= document.querySelector('input[type="submit"]');
+submit.value="SEND"
+
+// var item=document.querySelector('.list-group-item');
+// item.style.color='red';
+
+ var lastItem=document.querySelector('.list-group-item:last-child');
+ lastItem.style.color='transparent';
+
+ var secondItem=document.querySelector('.list-group-item:nth-child(2)');
+ secondItem.style.backgroundColor='green';
+ 
+//QUERYSELECTORSALL
+
+var titles=document.querySelectorAll('.title');
+console.log('titles');
+
+var odd=document.querySelectorAll('li:nth-child(odd)');
+var even=document.querySelectorAll('li:nth-child(even)');
+for(var i=0; i<odd.length;i++)
 {
-    li[i].style.backgroundColor='grey';
+    odd[i].style.backgroundColor='green';
+    even[i].style.backgroundColor='pink';
 }
 
